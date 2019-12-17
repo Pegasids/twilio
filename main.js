7faebed007d3e0d6802abd1ec155ea777a4f1853
Pegasids <tongtongcanopus@gmail.com>
2019-12-16 21:20:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const accountSid = 'AC8677fd075050fb6280d336ea4979c519';
-const authToken = '830af61976d96a9c23b1eb5b881683d5';
+const accountSid = '*';
+const authToken = '*';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
-      .create({body: 'Hi there!', from: '+13369398905', to: '+17788619922'})
+      .create({body: 'Hi there!', from: '*', to: '*'})
       .then(message => console.log(message.sid));
